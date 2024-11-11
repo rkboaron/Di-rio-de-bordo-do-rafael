e963158e8c50babb5a8d99bc777674413aea5957
silviosnjr <silviosnjr@gmail.com>
2024-11-11 03:57:59 -0300

    ajustes

--- app.js
+++ app.js
@@ -19,10 +19,9 @@ function criaCartao(categoria, pergunta, resposta){
 
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('active', respostaEstaVisivel);
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
     cartao.addEventListener('click', viraCartao)
     
-
     container.appendChild(cartao)
 }

baa05d585833e4a27d80055df3f87d7e1cb085fa
rkboaron <145038872+rkboaron@users.noreply.github.com>
2024-11-26 18:29:48 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,28 +6,30 @@ criaCartao(
 
 criaCartao(
     "Custo da Viagem", 
-    "custo da viagem",
-      "Passagem aérea (ida e volta): Aproximadamente R$ 1.500 a R$ 2.500, dependendo da cidade de origem e época do ano.\nHospedagem: Média de R$ 1.000 a R$ 2.500 por 7 dias em hotéis de 3 estrelas."
-); 
-   
+    "Qual foi o custo total da viagem por pessoa e como foi adquirido?", 
+    "Passagem aérea (ida e volta): Aproximadamente R$ 1.500 a R$ 2.500, dependendo da cidade de origem e época do ano.\nHospedagem: Média de R$ 1.000 a R$ 2.500 por 7 dias em hotéis de 3 estrelas."
+);
 
 criaCartao(
     'Fato Curioso', 
     'Qual curiosidade sobre o local visitado?', 
-    'Informe uma curiosidade interessante ou inusitada sobre a cidade, ponto turístico ou país visitado.' // Curiosidade não fornecida.
+    'Os pontos mais altos e mais baixos da América do Sul ficam na Argentina..
 );
 
 criaCartao(
     'Passeios', 
+    'Quais locais visitar?', 
     'Estádios, Bariloche, Patagônia.'
 );
 
 criaCartao(
-    'Prato Típico',  
+    'Prato Típico', 
+    'Conhece o prato típico?', 
     'Alfajor. Ingrediente principal: chocolate.'
 );
 
 criaCartao(
     'Ponto de Vista', 
+    'Qual foi sua opinião sobre a viagem?', 
     'País muito bom e lindo, porém faltou tempo para visitar outros pontos turísticos.'
 );

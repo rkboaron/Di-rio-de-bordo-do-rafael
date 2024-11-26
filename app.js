b0db76c3bc8cb5e7e7beb418316b48b90c034e58
Laura-Msalles <163336145+Laura-Msalles@users.noreply.github.com>
2024-11-26 06:15:32 -0300

    Update app.js código ajustado para fncionar

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-function criaCartao(categoria, pergunta, resposta){
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+function criaCartao(categoria, pergunta, resposta, imagem) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
-    cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-        <p>${pergunta}</p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-        <p>${resposta}</p>
-    </div>
-    </div>
-    `
+       cartao.innerHTML = `
+        <div class="cartao__conteudo">
+            <h3>${categoria}</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+            </div>
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+                ${imagem ? `<div class="cartao__imagem"><img src="${imagem}" alt="Imagem do álbum"></div>` : ''}
+            </div>
+        </div>
+    `;
 
-    let respostaEstaVisivel = false
+    let respostaEstaVisivel = false;
 
-    function viraCartao(){
-        respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('active', respostaEstaVisivel)
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
     }
-    cartao.addEventListener('click', viraCartao)
-    
-    container.appendChild(cartao)
+    cartao.addEventListener('click', viraCartao);
+
+    container.appendChild(cartao);
 }
- 
+criaCartao(
+    'Álbum de fotos', 
+    'Minhas memórias!', 
+    'Aqui está a montagem das suas fotos.',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqvVME05sJ2CAtZK6fpiKE2R8ONtmveZv03pPGB2-5QvbIJyNhd9JaPXvqcARoZEymW1w&usqp=CAU'
+);

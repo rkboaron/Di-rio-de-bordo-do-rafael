fe21aae2cf02f301326233fc1cee09382d6e0e2a
rkboaron <145038872+rkboaron@users.noreply.github.com>
2024-11-26 20:15:50 -0300

    Update perguntas.js
    restaurado o código

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
     'Diário de Bordo', 
     'Qual foi o destino?', 
-    'Argentina', 
+    'Informar o destino da viagem.', 
 )
 
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'Passagem aérea(ida e volta): Aproximadamente R$ 1.500 a R$ 2.500, -259.960,35 Peso Argentino , 433.267,25 Peso Argentino- dependendo da cidade de origem e época do ano. Hospedagem: Média de R$ 1.000 a R$ 2.500 por 7 dias em hotéis de 3 estrelas." ( 173.306,90 Peso Argentino, 433.267,25 Peso Argentino). 
+    'Informar o valor na moeda original do local e o valor convertido para R$. Informar se a aquisição foi via agência de viagens ou aquisição de forma independente '
 )
 
 criaCartao(
     'Fato Curioso', 
-    'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
-    'Os pontos mais altos e mais baixos da América do Sul ficam na Argentina.' 
+    'Uma curiosidade sobre cidade/ponto turístico/país visitado!', 
+    'Informar através de um breve texto uma curiosidade.'
 )
 
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Estádios de futebol, Bariloche conhecida por suas belezas naturais, Patagônia por suas bebidas, Buenos Aires por ser a capital.' 
+    'Informar os locais, breve resumo do que são.'
 )
 
 criaCartao(
     'Prato Típico', 
-    'Conhece o Alfajor?', 
-    'O alfajor, escolhi porque quando fui para la comprei e gostei muito desse prato, o ingrediente principal é chocolate e doce de leite.' 
+    'Conhece o "nome do prato"?', 
+    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
 )
 
 criaCartao(
     'Ponto de Vista', 
-    'O que eu achei da viagem para a Argentina?', 
-    'Achei a viagem maravilhosa , só da próxima vez pegaria uma pousada com mais dias, para conseguir ver tudo com mais calma e melhor.' 
+    'O que eu achei da viagem para ...?', 
+    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
 )

d6d63244acff115e8e24e3839a7ade65099f36d3
Laura-Msalles <163336145+Laura-Msalles@users.noreply.github.com>
2024-11-26 08:03:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -32,11 +32,3 @@ criaCartao(
     'Aqui está a montagem das suas fotos.',
     'https://cdn.temporadalivre.com/uploads/editor/pictures/8bc9aab356b1/content_Jardim_Bot%C3%A2nico_de_Curitiba_reabre_com_cuidados_red.jpg'
 );
- container.appendChild(cartao);
-}
-criaCartao(
-    'Álbum de fotos', 
-    'Minhas memórias!', 
-    'Aqui está a montagem das suas fotos.',
-    'https://cdn.temporadalivre.com/uploads/editor/pictures/8bc9aab356b1/content_Jardim_Bot%C3%A2nico_de_Curitiba_reabre_com_cuidados_red.jpg'
-);

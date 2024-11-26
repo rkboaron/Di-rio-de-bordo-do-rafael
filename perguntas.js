970c25349af8edc3fb8b98976b43c79737e78546
rkboaron <145038872+rkboaron@users.noreply.github.com>
2024-11-26 18:10:54 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,39 +1,35 @@
 criaCartao(
     'Diário de Bordo', 
     'Qual foi o destino?', 
-    'ARGENTINA.', 
+    'Informar o destino da viagem.', 
 )
 
 criaCartao(
-    'Custo da Viagem', 
-   Passagem aérea (ida e volta):
-Aproximadamente R$ 1.500 (259.911,15 Peso Argentino) a R$
-2.500 (433.073,50 Peso Argentino) , dependendo da cidade de
-origem e época do ano.
-
-Hospedagem: Média de R$ 1.000 (173.262,20 Peso Argentino) a R$
-2.500 (433.073,50 Peso Argentino)  por 7 dias em hotéis de 3
-estrelas.
+    "Custo da Viagem", 
+    "Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?", 
+    "descreva "
+)
 
 criaCartao(
     'Fato Curioso', 
-  A Argentina é conhecida pelo tango, uma dança e música tradicional que nasceu em Buenos Aires no final do século XIX.
-
+    'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
+    'Informar através de um breve texto uma curiosidade.'
 )
 
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-   estádios de futebol , patagônia que é uma parte conhecida pelas suas bebidas, bariloche conhecida pela suas paisagens 
+    'Informar os locais, breve resumo do que são.'
 )
 
 criaCartao(
     'Prato Típico', 
-    'alfajor 
-    principal ingrediente doce de leite
+    'Conhece o "nome do prato"?', 
+    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
 )
 
 criaCartao(
     'Ponto de Vista', 
-    'achei um país maravilhoso , porém faltou tempo para conhecer todas atrações e pontos turísticos, voltaria para ficar mais tempo e conhecer todos pontos turísticos'
+    'O que eu achei da viagem para ...?', 
+    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
 )

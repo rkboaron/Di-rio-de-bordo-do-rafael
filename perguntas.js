f086d00ce2a07531602bc67f3e9f8f83a150ccb0
rkboaron <145038872+rkboaron@users.noreply.github.com>
2024-11-26 19:51:44 -0300

    Update perguntas.js
    retorno do espaçamento final.

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Diário de Bordo',
-    'Qual foi o destino?',
-    'Argentina',
+    'Diário de Bordo', 
+    'Qual foi o destino?', 
+    'Argentina', 
 )
 
 criaCartao(
-    'Custo da Viagem',
-    'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?',
-    'Passagem aérea(ida e volta): Aproximadamente R$ 1.500 a R$ 2.500, -259.960,35 Peso Argentino , 433.267,25 Peso Argentino- dependendo da cidade de origem e época do ano. Hospedagem: Média de R$ 1.000 a R$ 2.500 por 7 dias em hotéis de 3 estrelas." ( 173.306,90 Peso Argentino, 433.267,25 Peso Argentino).
+    'Custo da Viagem', 
+    'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
+    'Passagem aérea(ida e volta): Aproximadamente R$ 1.500 a R$ 2.500, -259.960,35 Peso Argentino , 433.267,25 Peso Argentino- dependendo da cidade de origem e época do ano. Hospedagem: Média de R$ 1.000 a R$ 2.500 por 7 dias em hotéis de 3 estrelas." ( 173.306,90 Peso Argentino, 433.267,25 Peso Argentino). 
 )
 
 criaCartao(
-    'Fato Curioso',
-    'Uma curiosidade sobre cidade/ponto turístico/país cisitado!',
-    'Os pontos mais altos e mais baixos da América do Sul ficam na Argentina.'
+    'Fato Curioso', 
+    'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
+    'Os pontos mais altos e mais baixos da América do Sul ficam na Argentina.' 
 )
 
 criaCartao(
-    'Passeios',
+    'Passeios', 
     'Onde passear?', 
-    'Estádios de futebol, Bariloche conhecida por suas belezas naturais, Patagônia por suas bebidas, Buenos Aires por ser a capital.'
+    'Estádios de futebol, Bariloche conhecida por suas belezas naturais, Patagônia por suas bebidas, Buenos Aires por ser a capital.' 
 )
 
 criaCartao(
-    'Prato Típico',
-    'Conhece o Alfajor?',
-    'O alfajor, escolhi porque quando fui para la comprei e gostei muito desse prato, o ingrediente principal é chocolate e doce de leite.'
+    'Prato Típico', 
+    'Conhece o Alfajor?', 
+    'O alfajor, escolhi porque quando fui para la comprei e gostei muito desse prato, o ingrediente principal é chocolate e doce de leite.' 
 )
 
 criaCartao(
-    'Ponto de Vista',
-    'O que eu achei da viagem para a Argentina?',
-    'Achei a viagem maravilhosa , só da próxima vez pegaria uma pousada com mais dias, para conseguir ver tudo com mais calma e melhor.'
+    'Ponto de Vista', 
+    'O que eu achei da viagem para a Argentina?', 
+    'Achei a viagem maravilhosa , só da próxima vez pegaria uma pousada com mais dias, para conseguir ver tudo com mais calma e melhor.' 
 )
